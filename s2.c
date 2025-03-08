6c6284728edf8dc5f86ffa366b0e1e1bc116a6c8
GhetiuTudor <tudorghetiu1@gmail.com>
2025-03-08 10:01:27 +0200

    Update s2.c

--- s2.c
+++ s2.c
@@ -1,136 +1,155 @@
 #include<stdio.h>
 #include<malloc.h>
-#include<string.h>
 
 struct Masina {
-    int id;
-    int anFabricatie;
-    char* sofer;
-    float kilometriiParcursi;
-    char initialaProducator;
+	int id;
+	int anFabricatie;
+	char* sofer;
+	float kilometriiParcursi;
+	char initialaProducator;
 };
 
 struct Masina initializare(int id, int anFabricatie, const char* sofer, float kilometriiParcursi, char initialaProducator) {
-    struct Masina m;
-    m.id = id;
-    m.anFabricatie = anFabricatie;
-    if (sofer != NULL) {
-        m.sofer = (char*)malloc(strlen(sofer) + 1);
-        if (m.sofer) {
-            strcpy_s(m.sofer, strlen(sofer) + 1, sofer);
-        }
-    }
-    else {
-        m.sofer = NULL;
-    }
-    m.kilometriiParcursi = kilometriiParcursi;
-    m.initialaProducator = initialaProducator;
-    return m;
+	struct Masina m;
+	m.id = id;
+	m.anFabricatie = anFabricatie;
+	if (sofer != NULL)
+	{
+		m.sofer = (char*)malloc(strlen(sofer) + 1);
+		strcpy_s(m.sofer, strlen(sofer) + 1, sofer);
+	}
+	else
+	{
+		m.sofer = NULL;
+	}
+	m.kilometriiParcursi = kilometriiParcursi;
+	m.initialaProducator = initialaProducator;
+	return m;
 }
 
 void afisare(struct Masina m) {
-    printf("ID: %d \n", m.id);
-    printf("An fabricatie: %d \n", m.anFabricatie);
-    printf("Sofer: %s \n", m.sofer ? m.sofer : "N/A");
-    printf("Kilometrii Parcursi: %f \n", m.kilometriiParcursi);
-    printf("Initiala producator: %c \n", m.initialaProducator);
+	printf("ID: %d \n", m.id);
+	printf("An fabricatie: %d \n", m.anFabricatie);
+	printf("Sofer: %s \n", m.sofer);
+	printf("Kilometrii Parcursi : %f \n", m.kilometriiParcursi);
+	printf("Initiala producator: %c \n", m.initialaProducator);
 }
 
 void afisareVector(struct Masina* vector, int nrElemente) {
-    if (vector != NULL && nrElemente > 0) {
-        for (int i = 0; i < nrElemente; i++) {
-            afisare(vector[i]);
-            printf("\n");
-        }
-    }
+
+	if (vector != NULL && nrElemente > 0)
+	{
+		for (int i = 0; i < nrElemente; i++)
+		{
+			afisare(vector[i]);
+			printf("\n");
+		}
+	}
+
 }
 
 struct Masina* copiazaPrimeleNElemente(struct Masina* vector, int nrElemente, int nrElementeCopiate) {
-    struct Masina* vectorNou = NULL;
-    if (vector != NULL && nrElementeCopiate <= nrElemente && nrElementeCopiate > 0) {
-        vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * nrElementeCopiate);
-        if (vectorNou) {
-            for (int i = 0; i < nrElementeCopiate; i++) {
-                vectorNou[i] = initializare(vector[i].id, vector[i].anFabricatie, vector[i].sofer, vector[i].kilometriiParcursi, vector[i].initialaProducator);
-            }
-        }
-    }
-    return vectorNou;
+	struct Masina* vectorNou = NULL;
+	if (vector != NULL && nrElementeCopiate <= nrElemente && nrElementeCopiate > 0);
+	{
+		vectorNou = malloc(sizeof(struct Masina) * nrElementeCopiate);
+		for (int i = 0; i < nrElementeCopiate; i++)
+		{
+			vectorNou[i] = initializare(vector[i].id, vector[i].anFabricatie, vector[i].sofer, vector[i].kilometriiParcursi, vector[i].initialaProducator);
+
+		}
+	}
+	
+	return vectorNou;
 }
 
 void dezalocare(struct Masina** vector, int* nrElemente) {
-    if (*vector != NULL && *nrElemente > 0) {
-        for (int i = 0; i < *nrElemente; i++) {
-            if ((*vector)[i].sofer != NULL) {
-                free((*vector)[i].sofer);
-            }
-        }
-        free(*vector);
-        *vector = NULL;
-        *nrElemente = 0;
-    }
+	//dezalocam elementele din vector si vectorul
+	if ((*vector) != NULL & (*nrElemente) > 0)
+	{
+		for (int i = 0; i < (*nrElemente); i++)
+		{
+			free((*vector)[i].sofer);
+		}
+		free(*vector);
+		(*vector) = NULL;
+		(*nrElemente) = 0;
+	}
 }
 
-void copiazaMasiniCuMultiKM(struct Masina* vector, int nrElemente, float prag, struct Masina** vectorNou, int* dimensiune) {
-    if (vector != NULL && nrElemente > 0) {
-        (*dimensiune) = 0;
-        for (int i = 0; i < nrElemente; i++) {
-            if (vector[i].kilometriiParcursi > prag) {
-                (*dimensiune)++;
-            }
-        }
+void copiazaMasiniCuMultiKilometri(struct Masina* vector, char nrElemente, float prag, struct Masina** vectorNou, int* dimensiune) {
+	if (vector != NULL && nrElemente > 0)
+	{
+		(*dimensiune) = 0;
+		for (int i = 0; i < nrElemente; i++)
+		{
+			if (vector[i].kilometriiParcursi > prag)
+			{
+				(*dimensiune)++;
+			}
+		}
 
-        *vectorNou = (struct Masina*)malloc(sizeof(struct Masina) * (*dimensiune));
-        if (*vectorNou) {
-            int contor = 0;
-            for (int i = 0; i < nrElemente; i++) {
-                if (vector[i].kilometriiParcursi > prag) {
-                    (*vectorNou)[contor] = initializare(vector[i].id, vector[i].anFabricatie, vector[i].sofer, vector[i].kilometriiParcursi, vector[i].initialaProducator);
-                    contor++;
-                }
-            }
-        }
-    }
+		(*vectorNou) = malloc(sizeof(struct Masina) * (*dimensiune));
+		int contor = 0;
+		for (int i = 0; i < nrElemente; i++)
+		{
+			if (vector[i].kilometriiParcursi > prag)
+			{
+				(*vectorNou)[contor] = vector[i];
+				(*vectorNou)[contor].sofer = malloc(sizeof(char) * (strlen(vector[i].sofer) + 1));
+				strcpy_s((*vectorNou)[contor].sofer, strlen(vector[i].sofer) + 1, vector[i].sofer);
+				contor++;
+			}
+		}
+
+	}
+
+
 }
 
-struct Masina getPrimaMasinaDupaSofer(struct Masina* vector, int nrElemente, const char* conditie) {
-    for (int i = 0; i < nrElemente; i++) {
-        if (strcmp(vector[i].sofer, conditie) == 0) {
-            return initializare(vector[i].id, vector[i].anFabricatie, vector[i].sofer, vector[i].kilometriiParcursi, vector[i].initialaProducator);
-        }
-    }
-    return initializare(-1, 0, "", 0, '-');
+struct Masina getPrimaMasinaDupaSofer(struct Masina* vector, int nrElemente, const char* soferCautat) {
+	for (int i = 0; i < nrElemente; i++)
+	{
+		if (strcmp(soferCautat, vector[i].sofer) == 0)
+		{
+			return vector[i];
+		}
+	}
+	return initializare(-1, 0, NULL, 0, '-');
+	
 }
+	
+
 
 int main() {
-    int nrElemente = 3;
-    struct Masina* vector = (struct Masina*)malloc(sizeof(struct Masina) * nrElemente);
-    if (!vector) return 1;
+	int nrElemente = 3;
+	struct Masina* vector = malloc(sizeof(struct Masina) * nrElemente);
+	vector[0] = initializare(1, 2010, "Marian", 2100, 'F');
+	vector[1] = initializare(2, 2008, "Cristi", 1500.6, 'A');
+	vector[2] = initializare(3, 2017, "Marian", 7500, 'M');
 
-    vector[0] = initializare(1, 2000, "Ion", 5000, 'A');
-    vector[1] = initializare(2, 2001, "Ana", 3000, 'F');
-    vector[2] = initializare(3, 2002, "Stefan", 7500, 'M');
-    afisareVector(vector, nrElemente);
+	afisareVector(vector, nrElemente);
 
-    int nrElementeNoi = 3;
-    struct Masina* vectorNou = copiazaPrimeleNElemente(vector, nrElemente, nrElementeNoi);
-    printf("Masini copiate: \n");
-    afisareVector(vectorNou, nrElementeNoi);
+	int nrElementeCopiate = 1;
+	struct Masina* vectorNou = copiazaPrimeleNElemente(vector, nrElemente, nrElementeCopiate);
+	printf("Masini copiate: \n");
+	afisareVector(vectorNou, nrElementeCopiate);
 
-    dezalocare(&vector, &nrElemente);
+	dezalocare(&vectorNou, &nrElementeCopiate);
 
-    float prag = 3500;
-    copiazaMasiniCuMultiKM(vectorNou, nrElementeNoi, prag, &vector, &nrElemente);
-    printf("Masini cu multi km: \n");
-    afisareVector(vector, nrElemente);
+	float prag = 4000;
 
-    dezalocare(&vector, &nrElemente);
-    dezalocare(&vectorNou, &nrElementeNoi);
-
-    struct Masina primaMasina = getPrimaMasinaDupaSofer(vector, nrElemente, "Ana");
-    afisare(primaMasina);
-    free(primaMasina.sofer);
+	copiazaMasiniCuMultiKilometri(vector, nrElemente, prag, &vectorNou, &nrElementeCopiate);
+	printf("Masini cu multi km: \n");
+	afisareVector(vectorNou, nrElementeCopiate);
+	dezalocare(&vectorNou, &nrElementeCopiate);
 
 
-    return 0;
+	printf("Prima masina: \n");
+	struct Masina primaMasina = getPrimaMasinaDupaSofer(vector, nrElemente, "Ion");
+	afisare(primaMasina);
+
+	dezalocare(&vector, &nrElemente);
+
+	return 0;
 }
